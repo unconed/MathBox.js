35ee70258a49458e90eb13e9fc1bf5901af99168
ichuang <ichuang@mit.edu>
2014-04-04 14:30:36 -0400

    new build

--- build/MathBox-core.js
+++ build/MathBox-core.js
@@ -3677,9 +3677,16 @@ MathBox.Label.prototype = _.extend(new MathBox.Primitive(null), {
     var options = this.get(),
     // Axis vector direction for labels
     p = [0, 0, 0];
+
     p[options.facing] = 1;
+
     var labelTangent = this.labelTangent;
     labelTangent.set.apply(labelTangent, p);
+
+    var position = options.position;
+    var labelPoint = this.labelPoint;
+    labelPoint.set.apply(labelPoint, position);
+
     this.labels.show(true);
   },
 
@@ -3689,10 +3696,10 @@ MathBox.Label.prototype = _.extend(new MathBox.Primitive(null), {
       text = options.text,
       distance = options.distance,
       style = this.style,
-      labelTangent = this.labelTangent = new THREE.Vector3();
+    labelTangent = this.labelTangent = new THREE.Vector3(),
+      labelPoint  = this.labelPoint = new THREE.Vector3();
 
     var labelOptions = { dynamic: true, distance: distance };
-    var labelPoint  = new THREE.Vector3();
     labelPoint.set.apply(labelPoint, position)
     // label text callback
     var callback = function (i) {
